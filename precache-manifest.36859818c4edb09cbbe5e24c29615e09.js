4ab880d928ce6309c2c0927a0914d47a599e0943
CC <cici.j.zhang@gmail.com>
2020-09-17 18:30:59 -0400

    Updates

---
+++ precache-manifest.36859818c4edb09cbbe5e24c29615e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba4586b316ac559f7ebbfa4a5c308bf",
+    "url": "/coverletterMadeEasy/index.html"
+  },
+  {
+    "revision": "4c35ff5905fef8199f20",
+    "url": "/coverletterMadeEasy/static/css/main.05f1803f.chunk.css"
+  },
+  {
+    "revision": "b24ed840b9c74a656d32",
+    "url": "/coverletterMadeEasy/static/js/2.d822a378.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coverletterMadeEasy/static/js/2.d822a378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c35ff5905fef8199f20",
+    "url": "/coverletterMadeEasy/static/js/main.bc6b1e5e.chunk.js"
+  },
+  {
+    "revision": "bf170b7f5022d09a1b10",
+    "url": "/coverletterMadeEasy/static/js/runtime-main.5d4f0458.js"
+  }
+]);
